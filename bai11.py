a962c3204f276fd041938af727da8da56a406546
nguyentungminh-github <142736566+nguyentungminh-github@users.noreply.github.com>
2024-01-12 21:56:34 +0700

    Add files via upload

---
+++ bai11.py
@@ -0,0 +1,5 @@
+#Nhập vào 3 số a, b, c. In ra kết quả là tổng của ba số đó
+a=int(input())
+b=int(input())
+c=int(input())
+print (a+b+c)
---
+++ bai12.py
@@ -0,0 +1,8 @@
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+c = float(input("Nhập số c: "))
+d = (a + b) ** c
+if 100 <= d <= 200 :
+    print ("True")
+else :
+    print ("False")
