0366f013061bba705d9694d67871508a4e627094
nguyentungminh-github <142736566+nguyentungminh-github@users.noreply.github.com>
2024-01-12 21:25:05 +0700

    Add files via upload

---
+++ Bai10.py
@@ -0,0 +1,4 @@
+luong_thang_nay = float(input("Nhập lương tháng này: "))
+tien_dua_cho_vo = 0.9 * luong_thang_nay
+luong = luong_thang_nay - tien_dua_cho_vo
+print("Lương bạn giữ lại sau khi đưa cho vợ:", luong)
